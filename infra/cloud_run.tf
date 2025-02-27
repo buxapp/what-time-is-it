e3a6420510b84a712709aa48e253ac37835dd5cb
Victor Lopez <deanda.victor@protonmail.ch>
2025-02-27 16:25:29 -0600

    feat(cloudrun): Creation of SAs for CloudRun

---
+++ infra/cloud_run.tf
@@ -0,0 +1,16 @@
+resource "google_service_account" "cloud_run_sa" {
+  account_id   = "cloud-run-sa"
+  display_name = "Cloud Run Service Account"
+}
+
+resource "google_project_iam_member" "cloud_run_sa_roles" {
+  for_each = toset([
+    "roles/run.admin",
+    "roles/storage.objectViewer",
+    "roles/logging.logWriter",
+    "roles/monitoring.metricWriter",
+  ])
+  project = "bux-challenger"
+  role    = each.key
+  member  = "serviceAccount:${google_service_account.cloud_run_sa.email}"
+}
